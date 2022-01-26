7dc3f636e6d17b6dfb315fc00faa8dfa907e5276
Rihards11d <98041945+Rihards12d@users.noreply.github.com>
2022-01-26 12:25:42 +0000

    papilduzdevuma atrisinājums

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# 2.uzdevums.
-#Lietotājs ievada kvadrāta malas garumu centimetros (>5cm).<br>
-#Sastādīt programmas kodu, kas aprēķina, par cik procentiem mainīsies kvadrāta laukums, ja tā malas garums tiek mainīts par 5cm.<br>
-#Atbildi izvadīt veselos procentos.
-
+"""
 mala=float(input("Kvadrāta malas garums: "))
 if mala<=5:
   print("ievadi >5")
@@ -12,11 +8,17 @@ else:
   proc=s2*100/s1
   print(proc)
 
-
-  
-"""
-
 x%   = s2
 100% = s1
-
 """
+"""
+import math
+r=float(input("Riņķa rādiuss: "))
+hip=float(input("Vienādsānu taisnleņķa trijstūra hipotenūza: "))
+s_ri=math.pi*math.pow(r,2)
+s_tr=hip*(hip/2)/2
+starp=s_ri-s_tr
+starp1=round(starp,1)
+print(f"Riņķa laukums ir par {starp1} lielāks nekā vienādsānu taisnleņķa trijstūra laukums.")
+"""
+
